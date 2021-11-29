31b627e3cbbc80116a2598f84f6320956cc76d9b
GYCICT <90582133+GYCICT@users.noreply.github.com>
2021-11-29 15:52:54 +1100

    full path added
    with open('/scripts/docker-scripts/portainer/portainer-update', 'w') as portainer:

--- portainer/p-updater.py
+++ portainer/p-updater.py
@@ -18,7 +18,7 @@ pgithub_version = r.url.split('/')[-1]
 
 
 # Pull Current Version for Portainer
-with open('portainer-update') as portainer:
+with open('/scripts/docker-scripts/portainer/portainer-update') as portainer:
     p_file_content = portainer.read()
     p_file_match = re.search(r'(dockerversion=([0-9.]*))', p_file_content)
     p_file_version = p_file_match.group(2)
@@ -32,7 +32,7 @@ while True:
         url = 'https://github.com/portainer/portainer/releases/latest'
         r = requests.get(url)
         pgithub_version = r.url.split('/')[-1]
-        with open('portainer-update') as portainer:
+        with open('/scripts/docker-scripts/portainer/portainer-update') as portainer:
             p_file_content = portainer.read()
             p_file_match = re.search(r'(dockerversion=([0-9.]*))', p_file_content)
             p_file_version = p_file_match.group(2)
@@ -42,7 +42,7 @@ while True:
         print('Portainer requires an update')
         print('Updating From:',p_file_version)
         print('Latest Version:',pgithub_version)
-        with open('portainer-update') as portainer:
+        with open('/scripts/docker-scripts/portainer/portainer-update') as portainer:
             p_file_content = portainer.read()
             p_file_match = re.search(r'(dockerversion=([0-9.]*))', p_file_content)
             p_file_version = p_file_match.group(2)
@@ -52,7 +52,7 @@ while True:
 # Check who is higher
 if LooseVersion(pgithub_version) > LooseVersion(p_file_version):
     # print(f'github version ({github_version}) > file version ({file_version})')
-    with open('portainer-update', 'w') as portainer:
+    with open('/scripts/docker-scripts/portainer/portainer-update', 'w') as portainer:
         p_new_file_content = p_file_content.replace(p_file_match.group(1), f'dockerversion={pgithub_version}')
         portainer.write(p_new_file_content)
         portainer.close()
